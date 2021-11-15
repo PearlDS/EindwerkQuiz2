419bac58ff8526f049a247a68941297919c7988f
FlorianKeisse <FlorianJoeri.Keisse@student.intecbrussel.be>
2021-11-15 12:02:12 +0100

    basic files

---
+++ src/main/java/com/keisse/eindwerkquiz/EindwerkQuizApplication.java
@@ -0,0 +1,13 @@
+package com.keisse.eindwerkquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindwerkQuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EindwerkQuizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keisse/eindwerkquiz/EindwerkQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keisse.eindwerkquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindwerkQuizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
